e8054825e49cd1639df4c80a94e3cf06bffaaa95
Edet John <111274485+cyberohn2@users.noreply.github.com>
2024-08-28 23:58:06 +0100

    fixed NiN number typo

--- src/Components/UserForm.jsx
+++ src/Components/UserForm.jsx
@@ -137,7 +137,7 @@ const UserForm = memo(({ user, handleChange, errors }) => {
 
         <div className="flex flex-col items-start">
           <label className="mb-1 block text-gray-600 text-sm" htmlFor="nin">
-            NIN Number
+            NIN
           </label>
           <div className="flex gap-2 p-2 rounded-lg bg-white border border-[#F3F3F3] w-full">
             <input
